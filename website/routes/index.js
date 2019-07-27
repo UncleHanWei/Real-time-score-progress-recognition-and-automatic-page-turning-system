7459f3060ef9113fee2ea455343c8895b5ea91f1
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-07-27 16:19:47 +0800

    update routes

--- website/routes/index.js
+++ website/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Guitar' });
+});
+router.get('/Score', function(req, res, next) {
+  res.render('Score', { title: '看是哪個譜' });
+});
+router.get('/testScore', function(req, res, next) {
+  res.render('testScore', { title: '測試顯示譜' });
+});
+router.get('/pitchDetecter', function(req, res, next) {
+  res.render('pitchDetecter', { title: 'Guitar' });
+});
+router.get('/chordDetecter', function(req, res, next) {
+  res.render('chordDetecter', { title: 'Chord Detecer' });
 });
 
 module.exports = router;
