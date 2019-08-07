af437bbed1e2389bfea1fdc87100f647384dbe4e
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-08-07 17:26:18 +0800

    remove useless router

--- website/routes/index.js
+++ website/routes/index.js
@@ -2,20 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Guitar' });
 });
-router.get('/Score', function(req, res, next) {
-  res.render('Score', { title: '看是哪個譜' });
-});
-router.get('/testScore', function(req, res, next) {
-  res.render('testScore', { title: '測試顯示譜' });
-});
-router.get('/pitchDetecter', function(req, res, next) {
+router.get('/pitchDetecter', function (req, res, next) {
   res.render('pitchDetecter', { title: 'Guitar' });
 });
-router.get('/chordDetecter', function(req, res, next) {
+router.get('/chordDetecter', function (req, res, next) {
   res.render('chordDetecter', { title: 'Chord Detecer' });
 });
-
+router.get('/InsertScore', function (req, res, next) {
+  res.render('InsertScore', { title: '新增樂譜', msg: '' });
+})
 module.exports = router;
