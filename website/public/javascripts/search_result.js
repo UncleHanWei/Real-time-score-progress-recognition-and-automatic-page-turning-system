18ae2f7a3a0c3cb0f67a67e143c32cd26cdb80fa
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-08-10 00:13:56 +0800

    update css

--- website/public/javascripts/search_result.js
+++ website/public/javascripts/search_result.js
@@ -5,10 +5,10 @@ function setInputVal(id) {
 
 function _init(gotResult) {
   for (let i = 0; i < gotResult.length; i++) {
-    let htmlStr = '<div class="row"><div class="col-md-4 btn btn-secondary" id="';
+    let htmlStr = '<div class="row m-2"><div class="col-md-4 btn btn-secondary" id="';
     htmlStr += gotResult[i]._id;
     htmlStr += '" onclick="setInputVal(id)">';
-    htmlStr += gotResult[i].name;
+    htmlStr += gotResult[i].author + ' - ' + gotResult[i].name;
     htmlStr += '</div></div>';
     $('#resultList').append(htmlStr);
   }
