7615e240dcba32ee1773727fee7e458b1aa756ba
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-10-15 10:52:38 +0800

    add ssl

--- website/app.js
+++ website/app.js
@@ -3,11 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var httpApp = express();
 
 var indexRouter = require('./routes/index');
 var searchDB = require('./routes/searchDB');
 
 var app = express();
+
+
+var httpsOptions = {
+  key: fs.readFileSync('./ssl_key/ssl.key'),
+  cert: fs.readFileSync('./ssl_key/ssl.crt')
+};
+
+httpApp.set('port', process.env.PORT || 80);
+httpApp.get("*", function (req, res, next) {
+  res.redirect("https://" + req.headers.host + "/" + req.path);
+});
+
+app.set('port', process.env.PORT || 443);
+
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +43,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 網址列的變化
 app.use('/', indexRouter);
 app.use('/searchDB', searchDB);
+
+
+http.createServer(httpApp).listen(httpApp.get('port'), function() {
+  console.log('Express HTTP server listening on port ' + httpApp.get('port'));
+});
+
+https.createServer(httpsOptions, app).listen(app.get('port'), function() {
+  console.log('Express HTTPS server listening on port ' + app.get('port'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
