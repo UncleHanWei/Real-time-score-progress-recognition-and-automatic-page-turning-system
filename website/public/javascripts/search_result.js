78bfe71df900d49347f71385ddfe4fcffbf6f77d
UncleHanWei <s105213022@mail1.ncnu.edu.tw>
2019-11-05 11:26:06 +0800

    add mouse css

--- website/public/javascripts/search_result.js
+++ website/public/javascripts/search_result.js
@@ -5,7 +5,7 @@ function setInputVal(id) {
 
 function _init(gotResult) {
   for (let i = 0; i < gotResult.length; i++) {
-    let htmlStr = '<div class="row m-2"><div class="col-md-4 btn btn-secondary" id="';
+    let htmlStr = '<div class="row m-2"><div class="tbl-item col-md-4 btn btn-secondary" id="';
     htmlStr += gotResult[i]._id;
     htmlStr += '" onclick="setInputVal(id)">';
     htmlStr += gotResult[i].author + ' - ' + gotResult[i].name;
